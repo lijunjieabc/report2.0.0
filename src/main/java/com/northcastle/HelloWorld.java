3bad086a9b9486598cc9d0388786cc90434c1d25
lijunjieabc <2459454524@qq.com>
2022-10-30 23:40:38 +0800

    This is a GithubTest.

--- src/main/java/com/northcastle/HelloWorld.java
+++ src/main/java/com/northcastle/HelloWorld.java
@@ -2,7 +2,7 @@ package com.northcastle;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!!");
+        System.out.println("HelloWorld!!!");
         System.out.println("This is Maven!");
     }
 }
